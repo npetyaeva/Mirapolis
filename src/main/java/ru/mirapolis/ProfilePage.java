60bf4827eff1a5ceb729a02dd50174c0642c11ad
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-14 15:37:19 +0300

    Added fields

--- src/main/java/ru/mirapolis/ProfilePage.java
+++ src/main/java/ru/mirapolis/ProfilePage.java
@@ -1,5 +1,6 @@
 package ru.mirapolis;
 
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import java.util.Objects;
@@ -7,13 +8,15 @@ import java.util.Objects;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProfilePage {
+    private final SelenideElement avatarMenu = $(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div/div/div/div[2]"));
+    private final SelenideElement quitMenu = $(By.xpath("//*[@id=\"DropDownContainer28-DD\"]/div/div/div[3]/a/span[2]"));
 
     public boolean avatarFullName(String userFullName) {
         return Objects.requireNonNull($(By.className("avatar-full-name")).getAttribute("title")).contains(userFullName);
     }
 
     public void logOut() {
-        $(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div/div/div/div[2]")).click();
-        $(By.xpath("//*[@id=\"DropDownContainer28-DD\"]/div/div/div[3]/a/span[2]")).click();
+        $(avatarMenu).click();
+        $(quitMenu).click();
     }
 }

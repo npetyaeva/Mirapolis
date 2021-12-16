dd0305df95df0fe2b5c6dc4883b414e34fcebbee
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-16 23:09:01 +0300

    Changed locators for avatarMenu and quitMenu

--- src/main/java/ru/mirapolis/ProfilePage.java
+++ src/main/java/ru/mirapolis/ProfilePage.java
@@ -8,8 +8,8 @@ import java.util.Objects;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProfilePage {
-    private final SelenideElement avatarMenu = $(By.xpath("/html/body/div[1]/div/div/table/tbody/tr[1]/td/div[1]/div[2]/div[1]/div/div[2]/div/div/div/div/div/div/div[2]"));
-    private final SelenideElement quitMenu = $(By.xpath("//*[@id=\"DropDownContainer28-DD\"]/div/div/div[3]/a/span[2]"));
+    private final SelenideElement avatarMenu = $(By.className("avatar-full-name"));
+    private final SelenideElement quitMenu = $("[data-mira-id='Link33']");
 
     public boolean avatarFullName(String userFullName) {
         return Objects.requireNonNull($(By.className("avatar-full-name")).getAttribute("title")).contains(userFullName);
